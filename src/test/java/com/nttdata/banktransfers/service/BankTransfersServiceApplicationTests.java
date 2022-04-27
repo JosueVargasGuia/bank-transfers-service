35e4a6de91b654d82c257fca1f7489484b640059
JosueVargasGuia <44180784+JosueVargasGuia@users.noreply.github.com>
2022-04-26 23:47:52 -0500

    init

---
+++ src/test/java/com/nttdata/banktransfers/service/BankTransfersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.banktransfers.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransfersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
